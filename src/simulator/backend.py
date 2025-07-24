e46402317629ceeaa3c14a4dff59d1e905d228d4
Antoine BARBIER <antbarbi@f4r11s9.clusters.42paris.fr>
2025-07-24 13:58:59 +0200

    finished gameside logic, have to implement board size +1 for Wall representation for agent

--- src/simulator/backend.py
+++ src/simulator/backend.py
@@ -16,80 +16,120 @@ DIRECTION = {
     Action.RIGHT:   (0, 1)
 }
 
-def _random_cell(grid: np.ndarray, elem: str) -> tuple[int, int]:
-    if len(elem) != 1 or elem not in "WHSGR0":
-        raise Exception("Elem should be a single character")
-    while True:
-        row = np.random.randint(0, 10)
-        col = np.random.randint(0, 10)
-        if grid[row, col] == "0":
-            grid[row, col] = elem
-            return row, col
-
-def _snake_body(grid: np.ndarray, row: int, col: int) -> tuple[int, int]:
-    options = [(row - 1, col), (row, col - 1), (row + 1, col), (row, col + 1)]
-    valid_options = [
-        (r, c)
-        for r, c in options
-        if 0 <= r < grid.shape[0] and 0 <= c < grid.shape[1] and grid[r, c] == "0"
-    ]
-    chosen = random.choice(valid_options)
-    grid[chosen] = "S"
-    return chosen
-
-class Backend:
+class GameOver(Exception):
     def __init__(self):
-        self.grid = None
-        self.snake = []
+        super().__init__("Game Over.")
+
+
+def _get_random_coordinates() -> tuple[int, int]:
+    return np.random.randint(0, 10), np.random.randint(0, 10)
+
+def _get_random_adjacent(snake: list[tuple[int, int]], coor: tuple[int, int], grid_size: int = 10):
+    row, col = coor
+    options = [
+        (row-1, col), (row+1, col),
+        (row, col-1), (row, col+1)
+    ]
+    valid = [
+        (r, c) for r, c in options
+        if 0 <= r < grid_size and 0 <= c < grid_size and (r, c) not in snake
+    ]
+    return random.choice(valid)
+
+class Snake:
+    def __init__(self):
         self.reset()
-        print(self.grid)
-        print(self.snake)
+
+    def _init_snake(self) -> list[tuple[int, int]]:
+        coor = _get_random_coordinates()
+        self.snake.append(coor)
+        for _ in range(2):
+            self.snake.append(
+                _get_random_adjacent(self.snake, self.snake[-1])
+            )
+
+    def _random_cell(self) -> tuple[int, int]:
+        while True:
+            coor = _get_random_coordinates()
+            if coor not in self.snake and coor not in self.green_apples and coor != self.red_apple:
+                return coor
 
     def reset(self):
         # Init grid
-        self.grid = np.full((10, 10), '0', dtype='<U1')
+        self.grid: list[list]                       = np.full((10, 10), '0', dtype='<U1')
+        self.snake: list[tuple[int, int]]           = []
+        self.green_apples: list[tuple[int, int]]    = []
+        self.red_apple: tuple[int, int]             = ()
+
         # Init Snake
-        ret = _random_cell(self.grid, "H")
-        self.snake.append(ret)
-        for _ in "SS":
-            ret = _snake_body(self.grid, *ret)
-            self.snake.append(ret)
+        self._init_snake()
+
         # Init Apples
-        for c in "GGR":
-            _random_cell(self.grid, c) # 2 Green and 1 Red
-
-    def _udpate_snake(self, dr, dc, size: int = 0):
-        # Change head position
-        snake_head = (self.snake[0][0] + dr, self.snake[0][1] + dc)
-        self.snake.insert(snake_head)
-
-
+        for c in "GG":
+            self.green_apples.append(self._random_cell())
+        self.red_apple = self._random_cell()
 
     def step(self, action: Action) -> None:
         dr, dc = DIRECTION[action]
         head_row, head_col = self.snake[0]
         new_row, new_col = head_row + dr, head_col + dc
 
-        resize = 0
-        if not (0 <= new_row < self.grid.shape[0] and 0 <= new_col < self.grid.shape[1]):
-            print("game over")
-        if self.grid[new_row, new_col] == "S":
-            print("game over")
-        if self.grid[new_row, new_col] == "R":
-            self.snake.pop()
-            _random_cell(self.grid, "R")
-            resize = -1
-        if self.grid[new_row, new_col] == "G":
-            resize = 1
+        # Prevent moving into the second segment
+        if len(self.snake) > 1 and (new_row, new_col) == self.snake[1]:
+            print("Invalid move: cannot move into the second segment!")
+            return
 
+        coor = (new_row, new_col)
+        if not (0 <= new_row < self.grid.shape[0] and 0 <= new_col < self.grid.shape[1]):
+            raise GameOver()
+        elif coor in self.snake:
+            raise GameOver()
+        elif coor == self.red_apple:
+            if len(self.snake) <= 1:
+                raise GameOver()
+            self.snake.insert(0, coor)
+            self.snake = self.snake[:-2]
+            self.red_apple = self._random_cell()
+        elif coor in self.green_apples:
+            self.snake.insert(0, coor)
+            self.green_apples.remove(coor)
+            self.green_apples.append(self._random_cell())
+        else:
+            self.snake.insert(0, coor)
+            self.snake.pop()
+
+    def print(self):
+        color_map = {
+            "H": "\033[34mH\033[0m",  # Blue
+            "S": "\033[34mS\033[0m",  # Blue
+            "G": "\033[32mG\033[0m",  # Green
+            "R": "\033[31mR\033[0m",  # Red
+            "0": "0",                 # Default
+        }
+        grid = self.grid.copy()
+        red = self.red_apple
+        for index, elem in enumerate(self.snake):
+            grid[elem[0], elem[1]] = "H" if index == 0 else "S"
+        for elem in self.green_apples:
+            grid[elem[0], elem[1]] = "G"
+        grid[red[0], red[1]] = "R"
+        for row in grid:
+            print(" ".join(color_map[cell] for cell in row))
+        print()
 
     def get_observation():
         pass
 
-    def render():
-        pass
-
 
 if __name__ == "__main__":
-    backend = Backend()
-    backend.step(Action.DOWN)
+    backend = Snake()
+    INPUT_MAP = {
+        "w": Action.UP,
+        "s": Action.DOWN,
+        "a": Action.LEFT,
+        "d": Action.RIGHT
+    }
+    while True:
+        backend.print()
+        backend.step(INPUT_MAP[input()])
+        backend.print()
